673f9d1307ffa7f34189bf33a398117777efd69f
unknown <ogbemudiatimothy@gmail.com>
2020-04-12 15:10:42 +0100

    added umd module support

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
-function Uwadie(options, callback) {
-  function getPreformattedTime(options, callback) {
-    const defaults = {
-      setTimestamp: true,
-    };
-    const newDefaults = { ...defaults, ...options };
-    const preformattedTime = `${`${
-      /^0/.test(new Date().getHours())
-        ? '0' + new Date().getHours()
-        : new Date().getHours()
-    }:${
-      /^[0-9]/.test(new Date().getMinutes()) &&
-      new Date().getMinutes().length === 1
-        ? '0' + new Date().getMinutes()
-        : new Date().getMinutes()
-    }${
-      newDefaults.setTimestamp
-        ? new Date().getHours() >= '12'
-          ? 'PM'
-          : 'AM'
-        : ''
-    }`}`;
-    // remember to return here
-
-    callback(preformattedTime);
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], function () {
+      return factory(root);
+    });
+  } else if (typeof exports === 'object') {
+    module.exports = factory(root);
+  } else {
+    root.Uwadie = factory(root);
   }
-  getPreformattedTime(options, callback);
-}
-Uwadie({}, (preformattedTime) => {
-  console.log(preformattedTime);
-});
+})(
+  typeof global !== 'undefined'
+    ? global
+    : typeof window !== 'undefined'
+    ? window
+    : this,
+  function (window) {
+    const UwadieConstructor = function () {
+      const publicAPIS = {};
+      function getPreformattedTime(options, callback) {
+        const defaults = {
+          setTimestamp: true,
+        };
+        const newDefaults = { ...defaults, ...options };
+        const preformattedTime = `${`${
+          /^0/.test(new Date().getHours())
+            ? '0' + new Date().getHours()
+            : new Date().getHours()
+        }:${
+          /^[0-9]/.test(new Date().getMinutes()) &&
+          new Date().getMinutes().length === 1
+            ? '0' + new Date().getMinutes()
+            : new Date().getMinutes()
+        }${
+          newDefaults.setTimestamp
+            ? new Date().getHours() >= '12'
+              ? 'PM'
+              : 'AM'
+            : ''
+        }`}`;
+        // remember to return here
+
+        callback(preformattedTime);
+        return preformattedTime;
+      }
+      publicAPIS.getPreformattedTime = function (options, callback) {
+        getPreformattedTime(options, callback);
+      };
+
+      return publicAPIS;
+    };
+    return UwadieConstructor;
+  }
+);
